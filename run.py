b17cefd7c2e74bb8daea6f27c4ed353e955f1c94
MacBook Air 2020 <macbookair2020@MacBooks-MacBook-Air-2.local>
2026-03-07 06:27:33 +0300

    gaiah template

--- run.py
+++ run.py
@@ -1,6 +1,7 @@
-from app import create_app
+from flask import Flask, render_template
 
-app = create_app()
+app = Flask(__name__)
 
-if __name__ == "__main__":
-    app.run()
+@app.route("/")
+def home():
+    return render_template("index.html")
